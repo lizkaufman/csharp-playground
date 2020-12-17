86b771bb00c8c47b2582e085fa108a14665de4a8
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-12-17 09:18:41 +0000

    made calculator with switch

---
+++ calculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace fcc_tut_vid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the calculator!");
+            Console.WriteLine("Enter your first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter your operation (+, -, *, or /): ");
+            var operation = Console.ReadLine();
+            Console.WriteLine("Enter your second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Your answer is: ");
+
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine(num1 + num2);
+                    break;
+                case "-":
+                    Console.WriteLine(num1 - num2);
+                    break;
+                case "*":
+                    Console.WriteLine(num1 * num2);
+                    break;
+                case "/":
+                    Console.WriteLine(num1 / num2);
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+        }
+    }
+}
--- fcc-tut-vid/Program.cs
+++ fcc-tut-vid/Program.cs
@@ -6,12 +6,7 @@ namespace fcc_tut_vid
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Cube(3));
-        }
-        
-        static int Cube(int num) 
-        {
-            return num * num * num;
+            
         }
     }
 }
